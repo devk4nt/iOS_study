cbac5b826e92cdac7678fc7fceb5d5a8338de6d7
Kant <eileenyoo2@gmail.com>
2025-09-19 21:51:46 +0900

    feat: initialize ExCustomTollBottomBar project with essential files and configurations

---
+++ ExCustomTollBottomBar/ExCustomTollBottomBar/ExCustomTollBottomBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExCustomTollBottomBarApp.swift
+//  ExCustomTollBottomBar
+//
+//  Created by Kant on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ExCustomTollBottomBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
